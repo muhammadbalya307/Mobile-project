e256dcfa52922450623ebd77906eb449bf03c764
muhammad balya <50208607+muhammadbalya307@users.noreply.github.com>
2020-06-12 19:22:52 +0700

    Add files via upload

--- main/java/com/example/MobileProject/order.java
+++ main/java/com/example/MobileProject/order.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class home extends AppCompatActivity {
+public class order extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_order);
     }
     public void Masuk(View view) {
         Intent intent = new Intent(this, home.class);
